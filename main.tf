f4846826b00d90a469871870a22871f22ef5b8d3
Arun Siby <arun@gmail.com>
2023-12-07 07:37:30 +0000

    Backend security group created

--- main.tf
+++ main.tf
@@ -52,4 +52,47 @@ resource "aws_security_group" "frontend" {
   }
 }
 
+# Backend security group
+
+resource "aws_security_group" "backend" {
+  name        = "${var.project_name}-${var.project_env}-backend"
+  description = "${var.project_name}-${var.project_env}-backend"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    "Name" = "${var.project_name}-${var.project_env}-backend"
+  }
+}
+
 
